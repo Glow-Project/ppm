051e84921ebdeac079e39616225a1bf96a3c2b32
Tch1b0 <Jpour3006@gmail.com>
2022-02-11 16:51:50 +0100

    add `ConfigFile` property

--- pkg/utility/paths.go
+++ pkg/utility/paths.go
@@ -6,11 +6,26 @@ import (
 	"strings"
 )
 
+// Access different important paths in the GD project
 type Paths struct {
-	Root   string
+	/*
+		The root of the project.
+
+		If the project is a game, the root is on the same level
+		as the godot config files.
+
+		If the project is a plugin, the root is somewhere under /addons
+	*/
+	Root string
+
+	// The path to the addons folder
 	Addons string
+
+	// The path to the ppm.json config file
+	ConfigFile string
 }
 
+// Create a new paths instance from the root path
 func CreatePaths(rootPath string) Paths {
 	var addons string
 	if strings.HasSuffix(filepath.Dir(rootPath), "addons") {
@@ -20,11 +35,13 @@ func CreatePaths(rootPath string) Paths {
 	}
 
 	return Paths{
-		Root:   rootPath,
-		Addons: addons,
+		Root:       rootPath,
+		Addons:     addons,
+		ConfigFile: filepath.Join(rootPath, "ppm.json"),
 	}
 }
 
+// Create a new paths instance from the current working directory
 func CreatePathsFromCwd() (Paths, error) {
 	rootPath, err := os.Getwd()
 	if err != nil {
