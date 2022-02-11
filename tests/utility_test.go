e67c4a390191a7b345ed297404657619639da206
Tch1b0 <Jpour3006@gmail.com>
2022-02-11 13:33:23 +0100

    add basic utility tests

---
+++ tests/utility_test.go
@@ -0,0 +1,66 @@
+package tests
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/Glow-Project/ppm/pkg/utility"
+)
+
+const (
+	pathNotEqualMessage string = "Path \"%s\" is not equal to \"%s\""
+)
+
+func TestPaths(t *testing.T) {
+	t.Run("Create Paths instance as Game", func(t *testing.T) {
+		path := "/this/is/a/test"
+		addonsPath := filepath.Join(path + "/addons")
+		paths := utility.CreatePaths(path)
+		fmt.Println(path)
+		if paths.Root != path {
+			t.Errorf(pathNotEqualMessage, paths.Root, path)
+		}
+		if paths.Addons != addonsPath {
+			t.Errorf(pathNotEqualMessage, paths.Addons, addonsPath)
+		}
+	})
+
+	t.Run("Create Paths instance as Plugin", func(t *testing.T) {
+		path := "/this/is/a/test/addons/myplugin"
+		addonsPath := filepath.Join(path, "..")
+		paths := utility.CreatePaths(path)
+
+		if paths.Root != path {
+			t.Errorf(pathNotEqualMessage, paths.Root, path)
+		}
+		if paths.Addons != addonsPath {
+			t.Errorf(pathNotEqualMessage, paths.Addons, addonsPath)
+		}
+	})
+}
+
+func TestUtility(t *testing.T) {
+	t.Run("Test IndexOf function", func(t *testing.T) {
+		arr := []string{
+			"Test 1",
+			"Test 2",
+			"Test 3",
+		}
+
+		if utility.IndexOf("Test 1", arr) != 0 ||
+			utility.IndexOf("Test 2", arr) != 1 ||
+			utility.IndexOf("Test 3", arr) != 2 {
+			t.Fail()
+		}
+	})
+
+	t.Run("Test StringSliceContains function", func(t *testing.T) {
+		if !utility.StringSliceContains("Test", []string{"tEst", "setT", "Test"}) {
+			t.Fail()
+		}
+		if utility.StringSliceContains("Test", []string{"idka", "rhiugh", "zuroghz"}) {
+			t.Fail()
+		}
+	})
+}
