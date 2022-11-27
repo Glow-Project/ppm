1a47a9110213d1c67a051297df8bc2501113c224
Tch1b0 <jpour3006@gmail.com>
2022-11-27 11:40:34 +0100

    regex: add `IsAssetDependency` check

--- pkg/utility/regex.go
+++ pkg/utility/regex.go
@@ -3,9 +3,10 @@ package utility
 import "regexp"
 
 var (
-	urlRegex         = regexp.MustCompile(`https?:\/\/[a-zA-Z0-9_\-\.]+\.[a-zA-Z]{1,5}([a-zA-Z0-9_\/\-\=\&\?\:]+)*`)
-	githubUrlRegex   = regexp.MustCompile(`https?:\/\/github\.com(\/[a-zA-Z0-9_\-\=\&\?\:]+){2}`)
-	userAndRepoRegex = regexp.MustCompile(`[a-zA-Z0-9_\-]+\/[a-zA-Z0-9_\-]+`)
+	urlRegex             = regexp.MustCompile(`^https?:\/\/[a-zA-Z0-9_\-\.]+\.[a-zA-Z]{1,5}([a-zA-Z0-9_\/\-\=\&\?\:]+)*$`)
+	githubUrlRegex       = regexp.MustCompile(`^https?:\/\/github\.com(\/[a-zA-Z0-9_\-\=\&\?\:]+){2}$`)
+	userAndRepoRegex     = regexp.MustCompile(`^[a-zA-Z0-9_\-]+\/[a-zA-Z0-9_\-]+$`)
+	assetDependencyRegex = regexp.MustCompile(`^[a-zA-Z0-9\-\_]+$`)
 )
 
 func IsUrl(str string) bool {
@@ -18,4 +19,8 @@ func IsGithubRepoUrl(str string) bool {
 
 func IsUserAndRepo(str string) bool {
 	return userAndRepoRegex.MatchString(str)
+}
+
+func IsAssetDependency(str string) bool {
+	return assetDependencyRegex.MatchString(str)
 }
