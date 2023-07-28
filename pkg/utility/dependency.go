ee9bacadc8caf562e8cb8bc6f1c7fd8d7d0aa8f0
Tch1b0 <Jpour3006@gmail.com>
2023-07-28 16:01:16 +0200

    add `Version` field

--- pkg/utility/dependency.go
+++ pkg/utility/dependency.go
@@ -12,19 +12,30 @@ const (
 )
 
 type Dependency struct {
-	Identifier string `json:"identifier"`
-	Url        string `json:"url"`
-	Type       string `json:"type"`
+	Identifier string  `json:"identifier"`
+	Version    *string `json:"version"`
+	Url        string  `json:"url"`
+	Type       string  `json:"type"`
 }
 
 // create a new dependency struct from a string
 func DependencyFromString(str string) Dependency {
 	// the (ideally) unique identifier of the dependency
 	var identifier string
+	// the version of the dependency
+	var version *string = nil
 	// the url used for accessing the dependency
 	var url string
 	// the type of the dependency, GithubAsset | GDAsset
 	var t string
+
+	if strings.Contains(str, "@") {
+		tmp := strings.Split(str, "@")
+		v := tmp[len(tmp)-1]
+		version = &v
+
+		str = strings.Replace(str, "@"+v, "", 1)
+	}
 
 	if isGithubUrl := IsGithubRepoUrl(str); isGithubUrl || IsUserAndRepo(str) {
 		if isGithubUrl {
@@ -43,6 +54,7 @@ func DependencyFromString(str string) Dependency {
 
 	return Dependency{
 		Identifier: identifier,
+		Version:    version,
 		Url:        url,
 		Type:       t,
 	}
